10806760a7afe16b0ba30bea9ee739c09866e016
bhudgins <bhudgins@HARDING.EDU>
2018-02-14 16:02:45 -0600

    added some scripts

---
+++ MissionDemolition/Assets/Scripts/RigidBodySleep.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodySleep : MonoBehaviour {
+
+	void Start(){
+		RigidBody rb = GetComponent<Rigidbody>();
+		if(rb != null) rb.Sleep();
+	}
+}
